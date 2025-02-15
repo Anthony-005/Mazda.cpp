049da32fa365c425aaade3cbb8e0b81c1bf762b0
Anthony-005 <antomurugi225@gmail.com>
2025-02-15 18:03:03 +0300

    Update volumeofcylinder.cpp

--- volumeofcylinder.cpp
+++ volumeofcylinder.cpp
@@ -1,4 +1,9 @@
-
+//author: Anthony Ndungu
+// Date: 2025/15/02
+// Description: A program that calculates the volume of a cylinder based on the radius and height input by the user.
+// The program takes the radius and height of the cylinder as input, and calculates the volume using the formula: volume = π * radius^2 * height.
+// The program then outputs the volume of the cylinder.
+// Admission number:BSCIT-05-0097/2024
 #include<iostream>//cout,cin
 # define PI 3.14159
 using namespace std;//cout,cin,endl
